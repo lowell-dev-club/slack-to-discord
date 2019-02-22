26aae7c9f8d03a6653871f9eddf025250398c562
Savagecoder77 <savagecoder77@gmail.com>
2019-02-21 20:23:05 -0800

    simplify some of the code

--- bot.py
+++ bot.py
@@ -1,27 +1,12 @@
-import os
-import time
-import re
 from slackclient import SlackClient
 import config
 
-slack_client = config.token
-
-starterbot_id = None
-
-# constants
-RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-EXAMPLE_COMMAND = "do"
-MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+slack_token = config.token
+sc = SlackClient(slack_token)
 
 if __name__ == "__main__":
-    if slack_client.rtm_connect(with_team_state=False):
-        print("Starter Bot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
-        starterbot_id = slack_client.api_call("auth.test")["user_id"]
-        while True:
-            command, channel = parse_bot_commands(slack_client.rtm_read())
-            if command:
-                handle_command(command, channel)
-            time.sleep(RTM_READ_DELAY)
-    else:
-        print("Connection failed. Exception traceback printed above.")
+    sc.api_call(
+        "chat.postMessage",
+        channel="CG263QPBK",
+        text="Hello from Python! :tada:"
+    )
