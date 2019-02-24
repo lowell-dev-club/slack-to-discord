f6ecf7061257dd3fc381345f69162bb96c2126ee
Savagecoder77 <savagecoder77@gmail.com>
2019-02-23 18:02:03 -0800

    import re and add default message handler

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 # Imports
 from slackbot.bot import Bot, respond_to, listen_to
+import re
+
+@default_reply
+def my_default_handler(message):
+    message.reply('This command has not been coded. Ask a leader or check the command list')
 
 # Main function
 def main():
