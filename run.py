af94b2ae073f2d5e7c7b5e9666949c131d60c79f
Savagecoder77 <savagecoder77@gmail.com>
2019-02-23 17:57:20 -0800

    Starting bot that connects to slack

---
+++ run.py
@@ -0,0 +1,14 @@
+# Imports
+from slackbot.bot import Bot
+
+# Main function
+def main():
+    print('----------------------------')
+    print(' Lowell Dev CLub Bot Online')
+    print('----------------------------')
+    bot = Bot()
+    bot.run()
+
+# run the bot
+if __name__ == "__main__":
+    main()
