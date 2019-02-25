7db852c006d1678cd208fdcb559ae297e9da8a86
Savagecoder77 <savagecoder77@gmail.com>
2019-02-24 16:48:15 -0800

    remove extra line of code

--- run.py
+++ run.py
@@ -6,10 +6,9 @@ import sys
 
 # logging config
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
