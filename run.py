16c03ecb7833de1f71fae642f25d8a9d147596bf
Savagecoder77 <savagecoder77@gmail.com>
2019-02-24 17:31:51 -0800

    remove hi command and start announce command

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from slackbot.bot import Bot, respond_to, listen_to, default_reply
 import re
 import logging
 import sys
+from requests import post
+from json import dumps
+import config
 
 # logging config
 logger = logging.getLogger(__name__)
@@ -17,10 +20,21 @@ def my_default_handler(message):
     logger.info('default reply - message not understood')
     message.reply('This command has not been coded. Ask a leader or check the command list')
 
-@respond_to('hi', re.IGNORECASE)
-def hi(message):
-    logger.info('Hi command')
-    message.reply('Hello there!')
+@respond_to('announce (.*) (.*)', re.IGNORECASE)
+def stats(message, announcement=None, code=None):
+    message.reply('Announcing your announcement')
+    logger.info('announce command')
+
+    slack_data = {
+                 'text': '',
+                 'username': 'Dev Club',
+                 'icon_emoji': ':dev-club:'
+                 }
+
+    response = post(config.SLACK,
+                    data=dumps(slack_data),
+                    headers={'Content-Type': 'application/json'}
+                    )
 
 # Main function
 def main():
