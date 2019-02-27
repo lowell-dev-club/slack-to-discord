55cdc82c9099e1abfc9e5b6b59c4486b2b7fcd75
Savagecoder77 <savagecoder77@gmail.com>
2019-02-26 18:07:40 -0800

    slack webhook file to run slack webhook

---
+++ slackwebhook.py
@@ -0,0 +1,19 @@
+# Imports
+from requests import post
+from log import logger
+from config import SLACK
+
+def slackwebhook(message):
+    # Slack
+    slack_data = {
+                 'text': str(message),
+                 'username': 'Dev Club',
+                 'icon_emoji': ':dev-club:'
+                 }
+    logger.debug('create slack data')
+
+    response = post(SLACK,
+                    data=dumps(slack_data),
+                    headers={'Content-Type': 'application/json'}
+                    )
+    logger.debug('send slack data through slack webhook')
