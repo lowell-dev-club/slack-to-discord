9edb1a8bc05713de30ce7e51c00148e858b92063
Savagecoder77 <savagecoder77@gmail.com>
2019-03-04 17:45:16 -0800

    spacing

--- bot_commands.py
+++ bot_commands.py
@@ -4,11 +4,13 @@ import random
 """
 File for command code
 """
+
+
 def tip():
     """Get a random tip about using the bot."""
     tips = ["Only admins and the song requester can immediately skip songs. Everybody else will have to vote!",
            f"You can check out my source code here: https://github.com/lowell-dev-club/slack-to-discord"]
     index = random.randrange(len(tips))
-    return (f"**Tip #{index+1}:** {tips[index]})
+    return (f"** Tip  # {index+1}:** {tips[index]})
 
-def 
+def
