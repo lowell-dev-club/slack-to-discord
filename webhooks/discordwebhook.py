544265af11313dbb1c8558b58913324e182253eb
Savagecoder77 <savagecoder77@gmail.com>
2019-03-06 12:00:05 -0800

    remove asyncournous

--- webhooks/discordwebhook.py
+++ webhooks/discordwebhook.py
@@ -1,11 +1,11 @@
 # Imports
-import asyncio
 from log import logger
 from discord import Webhook, RequestsWebhookAdapter
 
-async def discordwebhook(message, WEBHOOK_ID, WEBHOOK_TOKEN):
+
+def discordwebhook(message, WEBHOOK_ID, WEBHOOK_TOKEN):
     webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=RequestsWebhookAdapter())
     logger.debug('create discord webhook data')
 
-    await webhook.send(message)
+    webhook.send(message)
     logger.debug('send announcement through discord webhook')
