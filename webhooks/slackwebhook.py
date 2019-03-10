60621e5e30da9496100b9fb30633be598fd57ed9
Savagecoder77 <savagecoder77@gmail.com>
2019-03-10 15:10:47 -0700

    fixed logging issue

--- webhooks/slackwebhook.py
+++ webhooks/slackwebhook.py
@@ -9,11 +9,9 @@ def slackwebhook(message, SLACK):
                  'username': 'Dev Club',
                  'icon_emoji': ':dev-club:'
                  }
-    logging.debug('create slack data')
 
     response = post(config.SLACK,
                     data=dumps(slack_data),
                     headers={'Content-Type': 'application/json'}
                     )
 
-    logging.debug('send slack data through slack webhook')
