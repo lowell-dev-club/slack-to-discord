0efb4b6e2f2de8484ee745e22cd5bc45dbef031f
Savagecoder77 <savagecoder77@gmail.com>
2019-03-10 15:39:35 -0700

    add new logger

--- webhooks/discordwebhook.py
+++ webhooks/discordwebhook.py
@@ -1,11 +1,14 @@
 # Imports
-import logging
 from discord import Webhook, RequestsWebhookAdapter
 
 
-def discordwebhook(message, WEBHOOK_ID, WEBHOOK_TOKEN):
-    webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=RequestsWebhookAdapter())
-    logging.debug('create discord webhook data')
+def discordwebhook(message, WEBHOOK_ID, WEBHOOK_TOKEN, logger):
+
+    webhook = Webhook.partial(WEBHOOK_ID,
+                              WEBHOOK_TOKEN,
+                              adapter=RequestsWebhookAdapter())
+
+    logger.debug('create discord webhook data')
 
     webhook.send(message)
-    logging.debug('send announcement through discord webhook')
+    logger.info('announcement sent through discord webhook')
--- webhooks/slackwebhook.py
+++ webhooks/slackwebhook.py
@@ -2,15 +2,18 @@
 from requests import post
 from json import dumps
 
-def slackwebhook(message, SLACK):
+
+def slackwebhook(message, SLACK, logger):
 
     slack_data = {
-                 'text': str(message),
-                 'username': 'Dev Club',
-                 'icon_emoji': ':dev-club:'
-                 }
+        'text': str(message),
+        'username': 'Dev Club',
+        'icon_emoji': ':dev-club:'
+    }
+    logger.debug('created slack data')
 
     response = post(SLACK,
                     data=dumps(slack_data),
                     headers={'Content-Type': 'application/json'}
                     )
+    logger.info('announcement sent on slack webhook')
